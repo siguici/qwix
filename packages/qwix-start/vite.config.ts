ae6c583124fd0f549ca230eb8fcc474586c32038
Sigui Kessé Emmanuel <siguici@proton.me>
2024-08-09 01:08:12 +0100

    :heavy_minus_sign: Remove vite-tsconfig-paths

--- packages/qwix-start/vite.config.ts
+++ packages/qwix-start/vite.config.ts
@@ -1,6 +1,5 @@
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => {
   return {
@@ -8,15 +7,6 @@ export default defineConfig(() => {
       qwikVite({
         csr: true,
       }),
-      tsconfigPaths(),
     ],
-    preview: {
-      headers: {
-        "Cache-Control": "public, max-age=600",
-      },
-    },
-    build: {
-      manifest: "manifest.json",
-    },
   };
 });
