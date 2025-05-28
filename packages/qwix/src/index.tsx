6b0937980631609b7693f7a63dbcc9d99ac9814f
⚡ Sigui 🇨🇮 <siguici@proton.me>
2025-05-28 17:03:49 +0100

    ♻️ Improve function names

--- packages/qwix/src/index.tsx
+++ packages/qwix/src/index.tsx
@@ -4,11 +4,11 @@ import type { Component, Props, Renderer, Template } from "./types";
 
 class QwixElement extends HTMLElement {}
 
-export function defineRenderer<T extends Props>(renderer: Renderer) {
-  return defineTemplate<T>(renderer.selector, renderer.template);
+export function registerRendererComponent<T extends Props>(renderer: Renderer) {
+  return registerComponentBySelector<T>(renderer.selector, renderer.template);
 }
 
-export function defineTemplate<T extends Props>(
+export function registerComponentBySelector<T extends Props>(
   selector: string,
   template: Template,
 ) {
@@ -16,21 +16,24 @@ export function defineTemplate<T extends Props>(
     customElements.define(selector, QwixElement);
   }
 
-  defineSelectorElements(selector);
-  defineTagComponents(
+  replaceSelectorAttributes(selector);
+  mountAllMatchingTags(
     selector,
     parse_fn_params(template),
     component$<T>((props: T) => template(props)),
   );
 }
 
-export function defineSelectorElements(selector: string) {
+export function replaceSelectorAttributes(selector: string) {
   for (const elt of document.querySelectorAll(`[${selector}]`)) {
-    defineSelectorElement(elt, selector);
+    replaceElementWithCustomTag(elt, selector);
   }
 }
 
-export function defineSelectorElement(element: Element, selector: string) {
+export function replaceElementWithCustomTag(
+  element: Element,
+  selector: string,
+) {
   element.removeAttribute(selector);
 
   const attrs = element.attributes;
@@ -52,17 +55,17 @@ export function defineSelectorElement(element: Element, selector: string) {
     : element.replaceWith(selectorElement);
 }
 
-export function defineTagComponents(
+export function mountAllMatchingTags(
   tagName: string,
   params: object,
   component: Component,
 ) {
   for (const element of document.getElementsByTagName(tagName)) {
-    defineElementComponent(element, params, component);
+    mountComponentToElement(element, params, component);
   }
 }
 
-export function defineElementComponent(
+export function mountComponentToElement(
   element: Element,
   params: object,
   component: Component,
@@ -89,10 +92,10 @@ export function defineElementComponent(
     }
   }
 
-  defineComponent(element, component, props, slot);
+  renderComponent(element, component, props, slot);
 }
 
-export function defineComponent<T extends Props>(
+export function renderComponent<T extends Props>(
   parent: Document | Element,
   Output: Component,
   props: T,
@@ -101,18 +104,20 @@ export function defineComponent<T extends Props>(
   return render(parent, <Output {...props}>{slot}</Output>);
 }
 
-export function qwix<T extends Props>(components: {
+export function registerQwixComponents<T extends Props>(components: {
   [key: string]: Template;
 }): void;
-export function qwix<T extends Props>(
+export function registerQwixComponents<T extends Props>(
   components: { selector: string; template: Template }[],
 ): void;
-export function qwix<T extends Props>(
+export function registerQwixComponents<T extends Props>(
   selector: string,
   template: Template,
 ): void;
-export function qwix<T extends Props>(renderer: Renderer): void;
-export function qwix<T extends Props>(
+export function registerQwixComponents<T extends Props>(
+  renderer: Renderer,
+): void;
+export function registerQwixComponents<T extends Props>(
   arg1:
     | string
     | Renderer
@@ -121,13 +126,13 @@ export function qwix<T extends Props>(
   arg2?: Template,
 ): void {
   if (typeof arg1 === "string" && typeof arg2 === "function") {
-    defineTemplate<T>(arg1, arg2);
+    registerComponentBySelector<T>(arg1, arg2);
     return;
   }
 
   if (Array.isArray(arg1)) {
     for (const renderer of arg1) {
-      defineRenderer<T>(renderer);
+      registerRendererComponent<T>(renderer);
     }
     return;
   }
@@ -137,17 +142,17 @@ export function qwix<T extends Props>(
       typeof arg1.selector === "string" &&
       typeof arg1.template === "function"
     ) {
-      defineRenderer<T>(arg1 as Renderer);
+      registerRendererComponent<T>(arg1 as Renderer);
       return;
     }
 
     for (const [selector, template] of Object.entries(arg1)) {
-      defineTemplate<T>(selector, template);
+      registerComponentBySelector<T>(selector, template);
     }
     return;
   }
 
-  throw new Error("Invalid arguments passed to qwix()");
+  throw new Error("Invalid arguments passed to registerQwixComponents()");
 }
 
-export default qwix;
+export default registerQwixComponents;
